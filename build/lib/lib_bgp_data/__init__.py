7f612e545e16a087adcfe0aba3f0acf99e946c9f
Justin Furuness <jfuruness@gmail.com>
2018-08-14 12:07:46 -0400

    removed useless files

--- build/lib/lib_bgp_data/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""This package contains a parser and database to parse bgp data"""
-
-from .database import Database
-from .parser import Parser
--- build/lib/lib_bgp_data/bgpstream/bgpstream_database/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""This package has db functionality for bgpstream events"""
-
-from .bgpstream_database import BGPStream_DB
