f1268389c8a16539bdc01bd3c73e9f2226ef1734
Justin Furuness <jfuruness@gmail.com>
2018-08-29 14:57:04 -0400

    added execute func for db

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='lib_bgp_data',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.1.1',
     author='Justin Furuness',
     author_email='jfuruness@gmail.com',
     url='https://github.com/jfuruness/lib_bgp_data.git',
