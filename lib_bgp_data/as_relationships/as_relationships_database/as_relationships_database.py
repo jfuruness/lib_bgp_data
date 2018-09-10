3783fe5ebc9a3db7ca02e652bd03023608de3c86
Justin Furuness <jfuruness@gmail.com>
2018-09-10 11:39:49 -0400

    db change

--- lib_bgp_data/as_relationships/as_relationships_database/as_relationships_database.py
+++ lib_bgp_data/as_relationships/as_relationships_database/as_relationships_database.py
@@ -23,65 +23,42 @@ class AS_Relationship_DB(AS_Relationships_DB_Wrapper):
         """Inserts a dict of as_relationship into db if it doesn't exist"""
 
         action = "Updated"
-        if not self._as_relationship_exists(row):
-            self._insert_as_relationship(row)
-            action = "Inserted"
+        if row.get("cone_as" is None:
+            if row.get("peer_as_1") is not None:
+                #insert into peers
+                self._insert_peers(row)
+            else:
+                #insert into customer providers
+                self._insert_customer_provider_pair(row)
         self.logger.debug("{} as_relationship".format(action))
 
-    def _get_as_relationship_data(self, row):
-        """Returns data dict for a sql query from as_relationship dict"""
-
-        customers = row.get("customer_as")
-        # Must convert the list of strings to a list of ints
-        if customers is not None and not isinstance(customers, list):
-            customers = [int(customers)]
-        elif customers is not None:
-            customers = [int(x) for x in customers]
-
-        data = [row.get("cone_as"),
-                customers,
-                row.get("provider_as"),
-                row.get("peer_as_1"),
-                row.get("peer_as_2"),
-                row.get("source")
-                ]
-        # Instead of empty string we want None
-        return [x if x != '' else None for x in data]
-
-    def _as_relationship_exists(self, row):
-        """Returns True if as_relationship doesn't exist, or False"""
-
+    def _insert_peers(self, row):
+        """insert peer to peer relationship into db"""
+        
         try:
-            # Check to make sure we didn't already insert this as_relationship
-            sql = """SELECT * FROM as_relationships
-                     WHERE cone_as = %s AND
-                     customer_as = %s AND
-                     provider_as = %s AND
-                     peer_as_1 = %s AND
-                     peer_as_2 = %s AND
-                     source = %s"""
-            self.cursor.execute(sql, self._get_as_relationship_data(row))
-            # No results, return false
-            results = self.cursor.fetchone()
-            self.logger.debug("Selected as_relationship")
-            if results is None:
-                return False
-            else:
-                return True
-        except Exception as e:
-            self.logger.error(
-                "Problem selecting as_relationship: {}".format(e))
-            raise e
-
-    def _insert_as_relationship(self, row):
-        """Inserts as_relationship data into database"""
-        try:
-            sql = """INSERT INTO as_relationships
-                     (cone_as, customer_as, provider_as, peer_as_1,
-                     peer_as_2, source)
-                     VALUES (%s, %s, %s, %s, %s, %s)"""
-            self.cursor.execute(sql, self._get_as_relationship_data(row))
+            sql = """INSERT INTO peers
+                     (peer_as_1, peer_as_2)
+                     VALUES (%s, %s)"""
+            data = [row.get("peer_as_1"), row.get("peer_as_2")]
+            data = [x if x != '' else None for x in data]
+            self.cursor.execute(sql, data)
             self.logger.debug("Inserted as_relationship")
         except Exception as e:
             self.logger.error("Problem inserting as_relationship".format(e))
             raise e
+
+    def _insert_customer_provider(self, row):
+        """insert customer provider relationship into db"""
+
+        try:
+            sql = """INSERT INTO customer_provider_pairs
+                     (cutomer_as, provider_as)
+                     VALUES (%s, %s)"""
+            data = [row.get("cutomer_as"), row.get("provider_as")]
+            data = [x if x != '' else None for x in data]
+            self.cursor.execute(sql, data)
+            self.logger.debug("Inserted as_relationship")
+        except Exception as e:
+            self.logger.error("Problem inserting as_relationship".format(e))
+            raise e
+
