95adfe5891a374dd1434664782e7370b6dbca2ab
Justin Furuness <jfuruness@gmail.com>
2019-06-20 00:17:56 -0400

    just discovered can run post install scripts

--- setup.py
+++ setup.py
@@ -32,4 +32,7 @@ setup(
             'parse_bgpstream.com = lib_bgpstream_parser.__main__:main'
         ]},
 )
-
+####https://jichu4n.com/posts/how-to-add-custom-build-steps-and-commands-to-setuppy/!!!
+#https://stackoverflow.com/questions/1321270/how-to-extend-distutils-with-a-simple-post-install-script/1321345#1321345
+#https://stackoverflow.com/questions/14441955/how-to-perform-custom-build-steps-in-setup-py
+1/0 # RUN INSTALL SCRIPT HERE AND CHANGE DOCS!!!
