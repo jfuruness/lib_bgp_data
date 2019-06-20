f2b292e2365e32f02c0fd5ff8e4cc581200e23fc
Justin Furuness <jfuruness@gmail.com>
2019-06-20 15:09:29 -0400

    updated pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='lib_bgp_data',
     packages=find_packages(),
-    version='0.1.29',
+    version='0.1.31',
     author='Justin Furuness',
     author_email='jfuruness@gmail.com',
     url='https://github.com/jfuruness/lib_bgp_data.git',
@@ -13,7 +13,6 @@ setup(
         'setuptools',
         'requests',
         'beautifulsoup4',
-        'fileinput',
         'flasgger',
         'Flask',
         'multiprocess',
@@ -25,7 +24,6 @@ setup(
         'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: Linux',
         'Programming Language :: Python :: 3'],
     entry_points={
         'console_scripts': [
@@ -35,4 +33,4 @@ setup(
 ####https://jichu4n.com/posts/how-to-add-custom-build-steps-and-commands-to-setuppy/!!!
 #https://stackoverflow.com/questions/1321270/how-to-extend-distutils-with-a-simple-post-install-script/1321345#1321345
 #https://stackoverflow.com/questions/14441955/how-to-perform-custom-build-steps-in-setup-py
-1/0 # RUN INSTALL SCRIPT HERE AND CHANGE DOCS!!!
+#1/0 # RUN INSTALL SCRIPT HERE AND CHANGE DOCS!!!
