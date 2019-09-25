a3cb0b502905f9119c0a0360cb3f45e09b8de0ca
jfuruness <jfuruness@gmail.com>
2019-09-25 13:12:34 -0400

    omg

--- lib_bgp_data/utils/utils.py
+++ lib_bgp_data/utils/utils.py
@@ -89,7 +89,10 @@ def now():
 def get_default_start():
     """Gets default start time, used in multiple places."""
 
-    return (get_default_end()-timedelta(days=1)).timestamp()
+    return (now()-timedelta(days=2)).replace(hour=0,
+                                             minute=0,
+                                             second=0,
+                                             microsecond=0).timestamp()
 
 
 def get_default_end():
@@ -99,11 +102,10 @@ def get_default_end():
     # the Caida API is actually broken. We've contacted them but
     # they have not fixed it even though they admitted it was incorrect
 
-    return (now()-timedelta(days=1)).timestamp().replace(hour=0,
-                                                         minute=0,
-                                                         second=0,
-                                                         microsecond=0)
-
+    return (now()-timedelta(days=1)).replace(hour=0,
+                                             minute=0,
+                                             second=0,
+                                             microsecond=0).timestamp()
 
 def set_common_init_args(self, args, paths=True):
     """Sets self attributes for arguments common across many classes"""
