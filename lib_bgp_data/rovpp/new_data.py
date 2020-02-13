e5a8729286f36a8df685f7661b796b40b9af4633
jfuruness <jfuruness@gmail.com>
2020-02-13 11:41:36 -0500

    Update new_data.py

--- lib_bgp_data/rovpp/new_data.py
+++ lib_bgp_data/rovpp/new_data.py
@@ -8,13 +8,13 @@ with open("result.json") as j:
         if pol_num == "0":
             continue
         else:
-            X = [1,2,3,4,5,10,20,30,40,60,80]
+            X = [1,10,20,30,40,60,80]
             new_data[pol_num] = {"X": X,
                                  "Y": []}
             adopting_l = g_dict["subprefix_hijack"]["rovpp_edge_ases"][pol_num]["data"]["_adopting"]["['trace_hijacked', 'trace_preventivehijacked']"]["Y"]
             collateral_l = g_dict["subprefix_hijack"]["rovpp_edge_ases"][pol_num]["data"]["_collateral"]["['trace_hijacked', 'trace_preventivehijacked']"]["Y"]
             # Collateral starts at 0but adopting doesn't so start at 1
-            for adopt_perc, adopting_h, collateral_h in zip(X, adopting_l, collateral_l[1:]):
+            for adopt_perc, adopting_h, collateral_h in zip(X, adopting_l, collateral_l):
                 new_data[pol_num]["Y"].append(adopting_h * adopt_perc / 100 + collateral_h * (1 - (adopt_perc / 100)))
     from pprint import pprint
 #    pprint(new_data)
@@ -27,9 +27,12 @@ with open("result.json") as j:
     plt.ylabel("Data plane % Hijacked")
     plt.plot(X, new_data["1"]["Y"], label="ROV", ls=styles[int(1)], marker=markers[int(1)])
     plt.plot(X, new_data["2"]["Y"], label="ROV++v1", ls=styles[int(2)], marker=markers[int(2)])
-    plt.plot(X, new_data["3"]["Y"], label="ROV++v2a", ls=styles[int(3)], marker=markers[int(3)])
-    plt.plot(X, new_data["4"]["Y"], label="ROV++v3", ls=styles[int(4)], marker=markers[int(4)])
     plt.plot(X, new_data["5"]["Y"], label="ROV++v2", ls=styles[int(5)], marker=markers[int(5)])
+#    plt.plot(X, new_data["3"]["Y"], label="ROV++v2a", ls=styles[int(3)], marker=markers[int(3)])
+    plt.plot(X, new_data["4"]["Y"], label="ROV++v3", ls=styles[int(4)], marker=markers[int(4)])
+
     plt.legend(loc="lower left")
 
     plt.show()
+        
+        
