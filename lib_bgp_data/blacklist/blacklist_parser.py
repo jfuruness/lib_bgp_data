3624ccf5c944f4227ca8ec8a2877f688b7f6467b
Nicholas Shpetner <nicholas.shpetner@uconn.edu>
2020-06-08 10:22:45 -0400

    Test now compares raw data vs table data to ensure the sizes are the same. Also adds documentation

--- lib_bgp_data/blacklist/blacklist_parser.py
+++ lib_bgp_data/blacklist/blacklist_parser.py
@@ -14,11 +14,9 @@ __status__ = "Development"
 
 import requests
 import io
-import os
 import gzip
 import re
 import csv
-import sys
 from io import StringIO
 from .tables import Blacklist_Table
 from ..base_classes import Parser
@@ -26,15 +24,15 @@ from ..utils import utils
 
 class Blacklist_Parser(Parser):
     
+    __slots__ = []
+
     def _run(self):
-        """Downloads and stores ASNs"""
-        with Blacklist_Table(clear=True) as table:
-            # Stuff goes here
-            raw = (self._parse_lists(self, self._get_blacklists(self)))
-            asns = self._format_dict(self, raw)
-            _csv_dir = f"{self.csv_dir}/roas.csv"
+        """Downloads and stores ASNs to table blacklist with columns uce2, uce3, spamhaus, and mit. Due to constraints when executing SQL, columns will be the size of the column with the most data, with None acting as filler forthe smaller columns where that source has no more ASNs on blacklist"""
+        with Blacklist_Table(clear=True) as _blacklist_table:
+            raw = (self._parse_lists(self._get_blacklists()))
+            asns = self._format_dict(raw)
+            _csv_dir = f"{self.csv_dir}/blacklist.csv"
             utils.rows_to_db(asns, _csv_dir, Blacklist_Table)
-            table._create_index()
 
 ######################
 ###Helper Functions###
@@ -42,26 +40,42 @@ class Blacklist_Parser(Parser):
 
     def _get_blacklists(self):
         """Gets blacklists from UCE level 2, UCE level 3, spamhaus, and the
-        MIT paper, makes a dict for each source, and puts the blacklist of          each source into the respective key's list.
+        MIT paper, makes a dict for each source, and attaches the 
+        blacklist of each source into the respective key as a string
         """
         sources = {'uce2':'http://wget-mirrors.uceprotect.net/rbldnsd-all/dnsbl-2.uceprotect.net.gz', 'uce3':'http://wget-mirrors.uceprotect.net/rbldnsd-all/dnsbl-3.uceprotect.net.gz', 'spamhaus':'https://www.spamhaus.org/drop/asndrop.txt', 'mit':'https://raw.githubusercontent.com/ctestart/BGP-SerialHijackers/master/prediction_set_with_class.csv'}
         output_str = dict()
         for typ in sources.keys():
             output_str[typ] = ''
+        # For each source in the sources dict, GET url and save string
+        # to dict.
         for source in sources.keys():
             downloaded_file = requests.get(sources[source])
+            # UCEPROTECT is inconsistent in that sometimes a python
+            # requested file will sometimes come as a txt, sometimes
+            # as a tarball that needs to be unzipped. We check this:
             if downloaded_file.headers['Content-Type'] == 'application/octet-stream':
                 decompressed = gzip.decompress(downloaded_file.content)
-                output_str[source] = decompressed.decode('utf-8')
+                # UCEPROTECT uses ISO-8859-1 for text, not utf-8
+                output_str[source] = decompressed.decode('ISO-8859-1')
             else:
+                # We have a txt and save to dict directly
                 output_str[source] = downloaded_file.text
         return output_str
 
     def _parse_lists(self, outputs: dict):
+        """For each source in outputs, takes the string attached to
+        the source and parses it for ASNs, then saves to dict with
+        sources as keys with a list of ASNs
+        """
         parsed = dict()
+        # For each source in outputs, parse for ASNs and save ASNs as
+        # list for each source in dict.
         for output in outputs.keys():
+            # If not a mit csv, just regex to find ASNs.
             if output != 'mit':
                 parsed[output] = re.findall(r'AS\d+', outputs[output])
+            # If mit csv, use csv utilities to ID malicious ASNs.
             if output == 'mit':
                 parsed[output] = []
                 fake_file = StringIO(outputs[output])
@@ -69,25 +83,42 @@ class Blacklist_Parser(Parser):
                 for row in mit_reader:
                     if row['HardVotePred'] == '1':
                         parsed[output].append(row['ASN'])
+        # For each key in parsed dictionary, remove duplicates and
+        # remove 'AS' from infront of ASN.
         for key in parsed.keys():
             parsed[key] = list(dict.fromkeys(parsed[key]))
             # Remove AS from the the numbers
             for i in range(len(parsed[key])):
-                result = parsed[key][i].replace('AS', '')
-                if result == None:
-                    print('Result is none')
-                if result == '':
-                    print('Result is blanks')
                 parsed[key][i] = parsed[key][i].replace('AS', '')
-        for key in parsed.keys():
-            if parsed[key] == []:
-                print(key + ' is empty')
         return parsed
 
     def _format_dict(self, parsed: dict):
-        """Takes a dict, with the header as the keys, and converts into a csv formatted string for input into database"""
-        fake_file = StringIO()
-        writer = csv.writer(fake_file)
-        for key, value in parsed.items():
-            writer.writerow([key, value])
-        return fake_file.getvalue()
+        """Takes a dict, with the header as the keys, and converts into a formatted list for input into database"""
+        # Constructor for list
+        unformatted = []
+        formatted = []
+        largest = 0
+        # For each key in parsed dict, add items of key to list as list
+        for source in parsed:
+            unformatted.append(parsed[source])
+        # For each list in unformatted list, do the following:
+        # See if this is the largest list we've worked with
+        # Convert items from str to int
+        for lst in range(len(unformatted)):
+            largest = max(largest, len(unformatted[lst]))
+            for i in range(len(unformatted[lst])):
+                unformatted[lst][i] = int(unformatted[lst][i])
+        # Now to format the list for use with rows_to_db, we gotta turn
+        # it 90 degrees, if you could visualize that. We make a list
+        # of lists, where each sublist is a row for our DB, and the
+        # sublist has the ASNs in the order [uce2, uce3, spamhaus, mit]
+        # If a source has no more data, we put None
+        for i in range(largest):
+            buff = []
+            for lst in range(len(unformatted)):
+                if len(unformatted[lst]) > i:
+                    buff.append(unformatted[lst][i])
+                else:
+                    buff.append(None)
+            formatted.append(buff)
+        return formatted
--- lib_bgp_data/blacklist/tests/test_blacklist_parser.py
+++ lib_bgp_data/blacklist/tests/test_blacklist_parser.py
@@ -12,22 +12,45 @@ __status__ = "Development"
 
 from psycopg2.errors import UndefinedTable
 import pytest
-
-from ..blacklist_parser import Blacklist_Parser as parser
+from ..blacklist_parser import Blacklist_Parser
 from ..tables import Blacklist_Table
 from ...database import Database
 
 @pytest.mark.blacklist_parser
 class Test_Blacklist_Parser:
-    """Tests all functions within the Blacklist Parser class."""
+    """Tests functions within the Blacklist Parser class."""
     @pytest.fixture(autouse=True)
     def setup(self):
+        """Parser setup and table deleted before every test"""
+        self.parser = Blacklist_Parser()
         with Database() as _db:
             _db.execute("DROP TABLE IF EXISTS blacklist")
-        
 
     def test_run(self):
-        parser._run(parser)
+        """Test that should make sure that there are no errors and that the table is populated with the correct amount of ASNs"""
+        # Run the parser
+        self.parser.run()
+        # Get the raw data. Done right after running parser for 
+        # better precision.
+        raw_dict = (self.parser._parse_lists(
+                     self.parser._get_blacklists()))
+        # Get # of ASNs in each source
+        raw_sizes = [len(raw_dict[source]) for source in raw_dict]
+        table_sizes = []
         with Database() as db:
-            asn_list = db.execute("SELECT asn FROM blacklist")
-            assert len(asn_list) > 0
+            # Columns in table
+            sources = ['uce2','uce3','spamhaus','mit']
+            # This loop will go through each column and ensure that
+            # the column is not empty and save the size of column to
+            # table_sizes.
+            for source in sources:
+                sql = "SELECT " + source + " FROM blacklist"
+                raw_list = db.execute(sql)
+                # List comprehension to remove None in column
+                asn_list = [asn for asn in raw_list if asn[source]]
+                assert len(asn_list) > 0
+                table_sizes.append(len(asn_list))
+        # Ensure that # of asns in raw data = size of columns.
+        # This has/will fail sometimes, but that is due to
+        # data provider inconsistencies/updates, from what I know.
+        assert raw_sizes == table_sizes
