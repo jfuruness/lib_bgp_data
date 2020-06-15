28d6a553b598999b622f15264b8e9ed217d51203
Samarth Kasbawala <samarth.kasbawala@uconn.edu>
2020-06-15 13:44:12 -0400

    add check for backup

--- lib_bgp_data/roas_parser/roas_automator.py
+++ lib_bgp_data/roas_parser/roas_automator.py
@@ -27,46 +27,81 @@ from ..utils import utils
 class TableIsEmpty(Exception):
     pass
 
+class BackupError(Exception):
+    pass
 
 class ROAs_Automator(ROAs_Parser):
+ 
     def _run(self):
         """Downloads and stores the roas from a json while properly maintaining
         the table in the database
         """
 
-        with ROAs_Table() as _roas_table:
+        with ROAs_Table() as table:
 
             # Check if the table exists and if it exists, make sure that the
             # table is not empty. If empty or table dne, restore it from the
             # table from the backup. Finally get the new data, store it, and
             # create a new backup
             try:
-                count = _roas_table.get_count()
+                count = table.get_count()
                 if count == 0:
                     raise TableIsEmpty()
             except (psycopg2.errors.UndefinedTable, TableIsEmpty):
                 try:
-                    _roas_table.clear_table()
-                    self._restore_table(_roas_table.name)
+                    table.clear_table()
+                    self._restore_table(table.name)
                 except subprocess.CalledProcessError:
                     # Enters this block if there is no compatible backup.
-                    # Probably eans we haven't created one yet
+                    # Probably means we haven't created one yet
                     pass
             finally:
-                # Store the roas in database, do not clear the table
+                # Get the roas that need to be inserted into the table
                 roas = self._format_roas(self._get_json_roas())
                 _csv_dir = f"{self.csv_dir}/roas.csv"
-                utils.rows_to_db(roas, _csv_dir, ROAs_Table, clear_table=False)
-                _roas_table.create_index()
 
-                # Backup the table
+                # Try backing up the table at most three times
                 try:
-                    self._backup_table(_roas_table.name)
-                except subprocess.CalledProcessError:
-                    # TODO: Will return an error if backup is not properly
-                    # constructed. But I have no idea what could cause this
-                    # error since pg_dump is being called with the proper
-                    # flags and arguments
+                    for _ in range(3):
+                        # Insert the rows into the table
+                        utils.clean_paths([self.path, self.csv_dir])
+                        utils.rows_to_db(roas, _csv_dir,
+                                         ROAs_Table, clear_table=False)
+                        table.create_index()
+
+                        # Create a backup, if it is successful, overwrite the
+                        # existing backup
+                        if self._backup_table(table.name):
+                            self._overwrite(table.name)
+                            break
+
+                        # If the backup is unsuccessful, restore the table
+                        # to the last backup
+                        else:
+                            try:
+                                table.clear_table()
+                                self._restore_table(table.name)
+                                table.create_index()
+                            except subprocess.CalledProcessError:
+                                # Enters this block if there is no backup file
+                                # to use
+                                pass
+
+                    # If after three attempts the backups are unsuccessful,
+                    # then just insert the new roas and don't backup.
+                    else:
+                        utils.clean_paths([self.path, self.csv_dir])
+                        utils.rows_to_db(roas, _csv_dir,
+                                         ROAs_Table, clear_table=False)
+                        table.create_index()
+
+                        # Raise a backup error
+                        raise BackupError()
+
+                except (subprocess.CalledProcessError, BackupError):
+                    # TODO: Enter this except block if there is an error
+                    # creating the backup. Will add functionality so that an
+                    # email is sent if the backups fail.
                     pass
 
 ########################
@@ -74,18 +109,52 @@ class ROAs_Automator(ROAs_Parser):
 ########################
 
     def _backup_table(self, table_name):
-        """This function runs the command to backup the roas table"""
+        """This funtion will backup the table and check the backup"""
 
-        bash = "sudo -i -u postgres << EOF\n"
-        bash += f"pg_dump -Fc -t {table_name} bgp > {table_name}.sql.gz\n"
-        bash += "EOF"
-        utils.run_cmds(bash)
+        # Get the number of rows in the table
+        with ROAs_Table() as table:
+            count = table.get_count()
+            
+        # Make a temporary backup
+        cmd = (f"sudo -i -u postgres << EOF\n"
+               f"pg_dump -Fc -t {table_name} bgp > {table_name}.temp.sql.gz\n"
+               f"EOF")
+        utils.run_cmds(cmd)
 
-    def _restore_table(self, table_name):
+        # Restore the table from the temporary backup we just made
+        with ROAs_Table() as table:
+            try:
+                table.clear_table()
+                self._restore_table(table_name, temp=True)
+            except:
+                # If there are any errors trying to restore the table,
+                # the backup was not successful
+                return False
+
+        # Get the new count of the table after the restore
+        with ROAs_Table() as table:
+            restore_count = table.get_count()
+
+        # Return True if the backup is consistent
+        return count == restore_count
+            
+    def _restore_table(self, table_name, temp=False):
         """This function runs the command to restore the table from the
         backup"""
 
-        bash = "sudo -i -u postgres << EOF\n"
-        bash += f"pg_restore -d bgp {table_name}.sql.gz\n"
-        bash += "EOF"
-        utils.run_cmds(bash)
+        # Build the bash command
+        cmd = "sudo -i -u postgres << EOF\n"
+        if temp:
+            cmd += f"pg_restore -d bgp {table_name}.temp.sql.gz\n"
+        else:
+            cmd += f"pg_restore -d bgp {table_name}.sql.gz\n"
+        cmd += "EOF"
+
+        utils.run_cmds(cmd)
+
+    def _overwrite(self, table_name):
+        cmd = (f"sudo -i -u postgres << EOF\n"
+               f"mv {table_name}.temp.sql.gz {table_name}.sql.gz\n"
+               f"EOF")
+        utils.run_cmds(cmd)
+
