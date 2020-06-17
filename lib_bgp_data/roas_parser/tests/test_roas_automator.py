43994b3065aa6ca7685e971e8f84dd9f8b0e14d0
Samarth Kasbawala <samarth.kasbawala@uconn.edu>
2020-06-17 13:44:01 -0400

    update tests

--- lib_bgp_data/roas_parser/tests/test_roas_automator.py
+++ lib_bgp_data/roas_parser/tests/test_roas_automator.py
@@ -15,10 +15,10 @@ __maintainer__ = "Justin Furuness"
 __email__ = "jfuruness@gmail.com"
 __status__ = "Development"
 
+import os
 import pytest
 import psycopg2
 import subprocess
-import time
 from unittest.mock import Mock, patch
 
 from ..roas_parser import ROAs_Parser
@@ -36,36 +36,64 @@ class Test_ROAs_Automator:
 
         self.parser = ROAs_Parser()
         self.automator = ROAs_Automator()
+
+        self.automator.backup_dir = "/nightly_runs/roas_backup/test/"
+        self.automator.temp_backup = (f"{self.automator.backup_dir}roas_test"
+                                      f"_temp.sql.gz")
+        self.automator.prev_backup = (f"{self.automator.backup_dir}roas_test"
+                                      f".sql.gz")
+
         with ROAs_Table() as _roas_table:
             _roas_table.clear_table()
 
     def teardown(self):
         """Backup files are deleted after each test"""
 
-        subprocess.run("sudo -i -u postgres rm roas_test.temp.sql.gz",
+        subprocess.run(f"rm {self.automator.temp_backup}",
                        capture_output=True,
                        shell=True)
-        subprocess.run("sudo -i -u postgres rm roas_test.sql.gz",
+        subprocess.run(f"rm {self.automator.prev_backup}",
                        capture_output=True,
                        shell=True)
+
+
+    def test__overwrite(self):
+        """Tests the _overwrite function"""
+
+        # Assert that a previous and temporary backup files don't exist
+        assert os.path.exists(self.automator.prev_backup) is False
+        assert os.path.exists(self.automator.temp_backup) is False
+
+        # Create a file that has the name of the temporary backup file
+        utils.run_cmds(f"touch {self.automator.temp_backup}")
+        assert os.path.exists(self.automator.temp_backup) 
+
+        # Overwrite the previous backup with the temporary one
+        self.automator._overwrite()
+
+        # Assert that temporary backup file no longer exits but the previous
+        # one does
+        assert os.path.exists(self.automator.temp_backup) is False
+        assert os.path.exists(self.automator.prev_backup)
 
     def test__restore_table(self):
         """Tests the _restore_table function
 
         We use a custom function since the _restore_table and _backup_table
         methods specify the database 'bgp'. Since we are working with the
-        'test' database, we simply replace 'bgp' with 'test'. The backup
-        function already has its own built in check, however it is still a
-        good idea to explicitly check.
+        'test' database, we simply replace 'bgp' with 'test'.
+
+        The restore function can either restore the table from the temp_backup
+        or from the previous backup. Both functionalities are tested.
         """
 
-        # Populate the database
+        # Populate the database using the roas_parser
         self.parser.run()
 
         with ROAs_Table() as _roas_table:
             # Get the data in the table and make a backup
             roas = _roas_table.get_all()
-            status = self._custom_backup(_roas_table.name)
+            self._custom_backup()
 
             # Drop the table and make sure it doesn't exist
             _roas_table.clear_table()
@@ -73,19 +101,28 @@ class Test_ROAs_Automator:
                 _roas_table.get_all()
 
             # Restore the table into the database
-            self._custom_restore(_roas_table.name, temp=True)
+            self._custom_restore(temp=True)
             _roas_table.create_index()
             restored_roas = _roas_table.get_all()
 
             # Assert that the roas are the same before and after the restore
             assert roas == restored_roas
 
-    def test__overwrite(self):
-        utils.run_cmds("sudo -i -u postgres touch roas_test.temp.sql.gz") 
-        self._custom_overwrite("roas")
-        with pytest.raises(subprocess.CalledProcessError):
-            utils.run_cmds("sudo -i -u postgres ls roas_test.temp.sql.gz")  
-        utils.run_cmds("sudo -i -u postgres ls roas_test.sql.gz") 
+            # Drop the table and make sure it doesn't exist
+            _roas_table.clear_table()
+            with pytest.raises(psycopg2.errors.UndefinedTable):
+                _roas_table.get_all()
+
+            # Overwrite the prev backup with the temp backup
+            self.automator._overwrite()
+
+            # Restore the table and check the contents
+            self._custom_restore()
+            _roas_table.create_index()
+
+            restored_roas = _roas_table.get_all()
+            assert roas == restored_roas
+
 
     def test__run(self):
         """Tests the _run function in the ROAs_Automator class"""
@@ -106,18 +143,14 @@ class Test_ROAs_Automator:
                        "_backup_table")
         restore_path = ("lib_bgp_data.roas_parser.roas_automator.ROAs_Automator."
                         "_restore_table")
-        overwrite_path = ("lib_bgp_data.roas_parser.roas_automator.ROAs_Automator."
-                          "_overwrite")
 
         with ROAs_Table(clear=True) as _roas_table:
             with patch(format_path, return_value=formatted_roas) as f_mock, \
                  patch(backup_path) as b_mock, \
-                 patch(restore_path) as r_mock, \
-                 patch(overwrite_path) as o_mock:
+                 patch(restore_path) as r_mock:
 
                 b_mock.side_effect = self._custom_backup
                 r_mock.side_effect = self._custom_restore
-                o_mock.side_effect = self._custom_overwrite
 
                 # Parses the roas and inserts them into the database
                 self.automator.run()
@@ -134,28 +167,24 @@ class Test_ROAs_Automator:
                        "_backup_table")
         restore_path = ("lib_bgp_data.roas_parser.roas_automator.ROAs_Automator."
                         "_restore_table")
-        overwrite_path = ("lib_bgp_data.roas_parser.roas_automator.ROAs_Automator."
-                          "_overwrite")
 
         with ROAs_Table(clear=True) as _roas_table:
             with patch(backup_path) as b_mock, \
-                 patch(restore_path) as r_mock, \
-                 patch(overwrite_path) as o_mock:
+                 patch(restore_path) as r_mock:
 
                 b_mock.side_effect = self._custom_backup
                 r_mock.side_effect = self._custom_restore
-                o_mock.side_effect = self._custom_overwrite
 
                 # Get the number of entries in the table, should be 0 at start
                 first = _roas_table.get_count()
                 assert first == 0
 
-                # Fill teh table and check that there are entries in the table
+                # Fill the table and check that there are entries in the table
                 self.automator.run()
                 second = _roas_table.get_count()
                 assert first < second
 
-                # Clear the table.We want to see if the restore works in the
+                # Clear the table. We want to see if the restore works in the
                 # run function.
                 _roas_table.clear_table() 
                 with pytest.raises(psycopg2.errors.UndefinedTable):
@@ -166,7 +195,7 @@ class Test_ROAs_Automator:
                 self.automator.run()
                 third = _roas_table.get_count()
                 assert second < third
-                assert (third - second) > 10000
+                assert (third - second) > 100000
 
     def test_failed_backup(self):
         """This function tests the behavior in case the backup doesn't work"""
@@ -176,15 +205,11 @@ class Test_ROAs_Automator:
                        "_backup_table")
         restore_path = ("lib_bgp_data.roas_parser.roas_automator.ROAs_Automator."
                         "_restore_table")
-        overwrite_path = ("lib_bgp_data.roas_parser.roas_automator.ROAs_Automator."
-                          "_overwrite")
 
         with ROAs_Table(clear=True) as _roas_table: 
-            with patch(restore_path) as r_mock, \
-                 patch(overwrite_path) as o_mock:
+            with patch(restore_path) as r_mock:
 
                 r_mock.side_effect = self._custom_restore
-                o_mock.side_effect = self._custom_overwrite
 
                 with patch(backup_path) as b_mock:
                     # Fill the table creating a backup normally, a backup file
@@ -207,16 +232,53 @@ class Test_ROAs_Automator:
                 # Suppose the table gets cleared, restoring from the last
                 # backup would not contain the data from the second call
                 _roas_table.clear_table()
-                self._custom_restore(_roas_table.name)
+                self._custom_restore()
 
                 assert count == _roas_table.get_count()
-                assert roas == _roas_table.get_all() 
+                assert roas == _roas_table.get_all()
+
+    def test_failed_backup_2(self):
+        """Another test to see the behavior of a failed backup
+
+        This test considers the case for when a backup fails and there is no
+        previous backup to restore to
+        """
+
+        # Get formatted roas
+        formatted_roas = self.automator._format_roas(self.automator._get_json_roas())
+
+        # Patch the format, backup, restore, and overwrite functions
+        format_path = ("lib_bgp_data.roas_parser.roas_automator.ROAs_Automator."
+                       "_format_roas")
+
+        # Also want to patch the backup, restore, and overwrite helper 
+        # functions because they specify the bgp database. When testing, we
+        # are working with test database
+        backup_path = ("lib_bgp_data.roas_parser.roas_automator.ROAs_Automator."
+                       "_backup_table")
+        restore_path = ("lib_bgp_data.roas_parser.roas_automator.ROAs_Automator."
+                        "_restore_table")
+
+        with ROAs_Table(clear=True) as _roas_table:
+            with patch(format_path, return_value=formatted_roas) as f_mock, \
+                 patch(backup_path, return_value=False) as b_mock, \
+                 patch(restore_path) as r_mock:
+
+                r_mock.side_effect = self._custom_restore
+
+                # Parses the roas and inserts them into the database
+                self.automator.run()
+
+                # Get the roas from the database and assert the roas match
+                assert formatted_roas == [list(row.values())
+                                          for row in _roas_table.get_all()]
+                assert os.path.exists(self.automator.prev_backup) is False
             
 ################################
 ### Patched Helper Functions ###
 ################################
 
-    def _custom_backup(self, table_name):
+    def _custom_backup(self):
         """This funtion will backup the table and check the backup"""
 
         # Get the number of rows in the table
@@ -224,16 +286,15 @@ class Test_ROAs_Automator:
             count = table.get_count()
 
         # Make a temporary backup
-        cmd = (f"sudo -i -u postgres << EOF\n"
-               f"pg_dump -Fc -t {table_name} test > {table_name}_test.temp.sql.gz\n"
-               f"EOF")
+        cmd = (f"sudo -i -u postgres "
+               f"pg_dump -Fc -t roas test > {self.automator.temp_backup}")
         utils.run_cmds(cmd)
 
         # Restore the table from the temporary backup we just made
         with ROAs_Table() as table:
             try:
                 table.clear_table()
-                self._custom_restore(table_name, temp=True)
+                self._custom_restore(temp=True)
             except:
                 # If there are any errors trying to restore the table,
                 # the backup was not successful
@@ -246,23 +307,14 @@ class Test_ROAs_Automator:
         # Return True if the backup is consistent
         return count == restore_count
 
-    def _custom_restore(self, table_name, temp=False):
+    def _custom_restore(self, temp=False):
         """This function runs the command to restore the table from the
         backup"""
 
-        # Build the bash command
-        cmd = "sudo -i -u postgres << EOF\n"
+        cmd = "sudo -i -u postgres "
         if temp:
-            cmd += f"pg_restore -d test {table_name}_test.temp.sql.gz\n"
+            cmd += f"pg_restore -d test {self.automator.temp_backup}"
         else:
-            cmd += f"pg_restore -d test {table_name}_test.sql.gz\n"
-        cmd += "EOF"
-
-        utils.run_cmds(cmd)
-
-    def _custom_overwrite(self, table_name):
-        cmd = (f"sudo -i -u postgres << EOF\n"
-               f"mv {table_name}_test.temp.sql.gz {table_name}_test.sql.gz\n"
-               f"EOF")
+            cmd += f"pg_restore -d test {self.automator.prev_backup}"
         utils.run_cmds(cmd)
 
