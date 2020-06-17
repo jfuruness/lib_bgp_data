96ca46efb6a84b4aa19a2ec566ce1e224546ff55
Samarth Kasbawala <samarth.kasbawala@uconn.edu>
2020-06-17 13:42:43 -0400

    fix bug if prev_backup not made already

--- lib_bgp_data/roas_parser/roas_automator.py
+++ lib_bgp_data/roas_parser/roas_automator.py
@@ -6,6 +6,23 @@
 The purpose of this class is to parse the roas and insert them into the
 database. This class will properly check to see if the database needs to be
 restored and will create a backup each time the _run method is called.
+
+First, the roas table in the database is checked. If this table is empty or
+dne, the table will be restored from a previous backup. If there is no
+previous backup, new data will be inserted using an emppty table.
+
+Next, the roas json is retrieved. The new data is inserted and a new backup is
+made. The table is then restored from the is backup. If number of rows in the
+table is the same after the insertion and then after the restore, the previous
+backup is overwritten. If the tempporary backup doesn't contain the same amount
+of data, we try creating a backup again. We try to make a backup at most three
+times. If a backup cannot be made for some reason, we do not overwrite the
+previous backup.
+
+This automator is designed to be run nightly on our server using cron.
+
+FEATURE TO ADD: Upon a failed backup, the script will send an email detailing
+the error.
 """
 
 __authors__ = ["Samarth Kasbawala"]
@@ -15,7 +32,7 @@ __maintainer__ = "Justin Furuness"
 __email__ = "jfuruness@gmail.com"
 __status__ = "Development"
 
-
+import os
 import psycopg2
 import subprocess
 from .tables import ROAs_Table
@@ -31,6 +48,9 @@ class BackupError(Exception):
     pass
 
 class ROAs_Automator(ROAs_Parser):
+    backup_dir = "/nightly_runs/roas_backup/"
+    temp_backup = f"{backup_dir}roas_temp.sql.gz"
+    prev_backup = f"{backup_dir}roas.sql.gz"
  
     def _run(self):
         """Downloads and stores the roas from a json while properly maintaining
@@ -48,13 +68,9 @@ class ROAs_Automator(ROAs_Parser):
                 if count == 0:
                     raise TableIsEmpty()
             except (psycopg2.errors.UndefinedTable, TableIsEmpty):
-                try:
+                if os.path.exists(self.prev_backup):
                     table.clear_table()
-                    self._restore_table(table.name)
-                except subprocess.CalledProcessError:
-                    # Enters this block if there is no compatible backup.
-                    # Probably means we haven't created one yet
-                    pass
+                    self._restore_table()
             finally:
                 # Get the roas that need to be inserted into the table
                 roas = self._format_roas(self._get_json_roas())
@@ -71,22 +87,23 @@ class ROAs_Automator(ROAs_Parser):
 
                         # Create a backup, if it is successful, overwrite the
                         # existing backup
-                        if self._backup_table(table.name):
-                            self._overwrite(table.name)
+                        if self._backup_table():
+                            self._overwrite()
                             break
 
                         # If the backup is unsuccessful, restore the table
                         # to the last backup
-                        else:
-                            try:
-                                table.clear_table()
-                                self._restore_table(table.name)
-                                table.create_index()
-                            except subprocess.CalledProcessError:
-                                # Enters this block if there is no backup file
-                                # to use
-                                pass
+                        if os.path.exists(self.prev_backup):
+                            table.clear_table()
+                            self._restore_table()
+                            table.create_index()
 
+                        # Enters this block if there is no previous backup,
+                        # need to make the table empty so that data can tried
+                        # to be inserted again
+                        else:
+                            table.execute("DELETE FROM roas")
+                            
                     # If after three attempts the backups are unsuccessful,
                     # then just insert the new roas and don't backup.
                     else:
@@ -108,7 +125,7 @@ class ROAs_Automator(ROAs_Parser):
 ### Helper Functions ###
 ########################
 
-    def _backup_table(self, table_name):
+    def _backup_table(self):
         """This funtion will backup the table and check the backup"""
 
         # Get the number of rows in the table
@@ -116,16 +133,15 @@ class ROAs_Automator(ROAs_Parser):
             count = table.get_count()
             
         # Make a temporary backup
-        cmd = (f"sudo -i -u postgres << EOF\n"
-               f"pg_dump -Fc -t {table_name} bgp > {table_name}.temp.sql.gz\n"
-               f"EOF")
+        cmd = (f"sudo -i -u postgres "
+               f"pg_dump -Fc -t roas bgp > {self.temp_backup}")
         utils.run_cmds(cmd)
 
         # Restore the table from the temporary backup we just made
         with ROAs_Table() as table:
             try:
                 table.clear_table()
-                self._restore_table(table_name, temp=True)
+                self._restore_table(temp=True)
             except:
                 # If there are any errors trying to restore the table,
                 # the backup was not successful
@@ -138,23 +154,20 @@ class ROAs_Automator(ROAs_Parser):
         # Return True if the backup is consistent
         return count == restore_count
             
-    def _restore_table(self, table_name, temp=False):
+    def _restore_table(self, temp=False):
         """This function runs the command to restore the table from the
         backup"""
 
-        # Build the bash command
-        cmd = "sudo -i -u postgres << EOF\n"
+        cmd = "sudo -i -u postgres "
         if temp:
-            cmd += f"pg_restore -d bgp {table_name}.temp.sql.gz\n"
+            cmd += f"pg_restore -d bgp {self.temp_backup}"
         else:
-            cmd += f"pg_restore -d bgp {table_name}.sql.gz\n"
-        cmd += "EOF"
-
+            cmd += f"pg_restore -d bgp {self.prev_backup}"
         utils.run_cmds(cmd)
 
-    def _overwrite(self, table_name):
-        cmd = (f"sudo -i -u postgres << EOF\n"
-               f"mv {table_name}.temp.sql.gz {table_name}.sql.gz\n"
-               f"EOF")
+    def _overwrite(self):
+        """This function overwrites the previous backup"""
+
+        cmd = f"mv {self.temp_backup} {self.prev_backup}"
         utils.run_cmds(cmd)
 
