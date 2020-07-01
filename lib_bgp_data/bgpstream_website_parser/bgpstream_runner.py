6b63a19d3022d1252acd5d1258bd1487332c7e1f
Tony Zheng <mr.tonyzheng@gmail.com>
2020-07-01 00:48:23 -0400

    email alert and unit tests

--- lib_bgp_data/bgpstream_website_parser/bgpstream_runner.py
+++ lib_bgp_data/bgpstream_website_parser/bgpstream_runner.py
@@ -19,11 +19,15 @@ Edit cron jobs by running this command:
 sudo crontab -e
 
 Then add this line:
-0 4 * * * /ext/env/bin/python /ext/lib_bgp_data/lib_bgp_data/bgpstream_website_parser/bgpstream_runner.py
-0 4 * * * /ext/env/bin/lib_bgp_data --bgpstream_website_runner
-/new_hires/tony/env/bin/lib_bgp_data --relationships_parser
-For other machines, this is a helpful article:
+0 4 * * * /nightly_runs/env/bin/lib_bgp_data --bgpstream_website_runner
+
+For reconfiguring cron, this is a useful article:
 https://medium.com/@gavinwiener/how-to-schedule-a-python-script-cron-job-dea6cbf69f4e
+
+In addition, an email is sent if the runner restores a table and it is still
+somehow empty. Either the restore process could have failed, or the backup
+file is somehow corrupted. Please check the the backup and the database in
+such a situation.
 """
 
 import os
@@ -31,10 +35,11 @@ from subprocess import check_call
 
 import psycopg2
 
-from lib_bgp_data.bgpstream_website_parser.bgpstream_website_parser import BGPStream_Website_Parser
-from lib_bgp_data.bgpstream_website_parser.tables import Hijacks_Table, Leaks_Table, Outages_Table
+from ..base_classes import Parser
+from ..utils import utils
+from .bgpstream_website_parser import BGPStream_Website_Parser
+from .tables import Hijacks_Table, Leaks_Table, Outages_Table
 
-from base_classes import Parser
 
 __author__ = "Tony Zheng"
 __credits__ = ["Tony Zheng"]
@@ -44,39 +49,56 @@ __email__ = "jfuruness@gmail.com"
 __status__ = "Development"
 
 
-class Runner: BGPStream_Website_Runner(Parser):
+class BGPStream_Website_Runner(Parser):
     table_list = [Hijacks_Table, Leaks_Table, Outages_Table]
-    backup_dir = '/var/lib/bgpstream_website_parser/backups/'
+    backup_dir = '/nightly_runs/bgpstream_backup/'
 
-    def run(self):
-        # making the backup directory if doesn't exist. usually first time running
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.section = self.kwargs['section']
+
+    def _run(self):
+        try:
+            for table in self.table_list:
+                backup = f'{table.name}.sql.gz'
+                with table() as t:
+                    try:
+                        count = t.get_count()
+                    except psycopg2.errors.UndefinedTable:
+                        self.restore(backup, t)
+
+                    if count == 0:
+                        self.restore(backup, t)
+
+            BGPStream_Website_Parser()._run()
+            for table in self.table_list:
+                self.backup(table)
+
+        except AssertionError:
+            subject = f'[{utils.now()}] BGPStream_Website_Parser Error'
+            body = ('Nightly run of BGPStream_Website_parser failed. '
+                    'Either restoring failed or a backup is corrupted. '
+                    'Please check the backups and the live db.')
+            utils.send_email(subject, body)
+
+    def backup(self, table):
+        """Makes a new backup if live table has more data than old backup."""
+
+        prev_backup = os.path.join(self.backup_dir, f'{table.name}.sql.gz')
+        tmp_backup = os.path.join(self.backup_dir, 'temp.sql.gz')
+       
+        # making the backup directory if doesn't exist. (e.g. first time)
         if not os.path.exists(self.backup_dir):
             os.mkdir(self.backup_dir)
 
-        for table in self.table_list:
-            backup = f'{table.name}.sql.gz'
-            with table() as t:
-                try:
-                    count = t.get_count()
-                except psycopg2.errors.UndefinedTable:
-                    self.restore(backup)
-            if count == 0:
-                self.restore(backup)
-
-        BGPStream_Website_Parser().run()
-        for table in self.table_list:
-            self.backup(table)
-
-    def backup(self, table):
-        prev_backup = f'{self.backup_dir}{table.name}.sql.gz'
-        tmp_backup = f'{self.backup_dir}temp.sql.gz'
-       
-        # this is actually the first time backing up
+        # if previous backups don't exists, make backups right now
         if not os.path.exists(prev_backup):
-            self.pg_cmd(f'pg_dump -Fc -t {table.name} bgp > {prev_backup}')
+            self.pg_cmd(f'pg_dump -Fc -t {table.name} '
+                        f'{self.section} > {prev_backup}')
  
         # make a temp backup of live table
-        self.pg_cmd(f'pg_dump -Fc -t {table.name} bgp > {tmp_backup}')
+        self.pg_cmd(f'pg_dump -Fc -t {table.name} '
+                    f'{self.section} > {tmp_backup}')
 
         with table() as db:
             # copy live table
@@ -84,31 +106,35 @@ class Runner: BGPStream_Website_Runner(Parser):
             db.execute(f'CREATE TABLE temp AS TABLE {table.name}')
 
             # restore previous backup
-            self.restore(prev_backup)
+            self.restore(prev_backup, db)
             count_prev = db.get_count()
             
             # restore temp
-            self.restore(tmp_backup)
+            self.restore(tmp_backup, db)
             count_tmp = db.get_count()
 
             # new backup is larger, save as the most up-to-date backup
             if count_tmp > count_prev:
-                self.pg_cmd(f'mv {tmp_backup} {prev_backup}')
+                check_call(f'mv {tmp_backup} {prev_backup}', shell=True)
 
             # restore live table
             db.execute(f'DROP TABLE {table.name}')
             db.execute(f'CREATE TABLE {table.name} AS TABLE temp')
             db.execute('DROP TABLE temp')
 
-    def restore(self, backup):
-        self.pg_cmd(f'pg_restore -c --if-exists -d bgp {backup}')
-        # check restore is successful
+            if os.path.exists(tmp_backup):
+                os.remove(tmp_backup)
+
+    def restore(self, backup, table):
+        """Restores backup into clean table, then checks table is nonempty."""
+
+        self.pg_cmd(f'pg_restore -c --if-exists -d {self.section} {backup}')
+         
+        assert table.get_count() > 0, ('Table is still empty. '
+                                       'Restore or backup must have failed.') 
 
     def pg_cmd(self, cmd: str):
+        """executes cmd as postgres user"""
         check_call(f'sudo -i -u postgres {cmd}', shell=True)
-
- 
-if __name__=='__main__':
-    Runner().run()
 
 
