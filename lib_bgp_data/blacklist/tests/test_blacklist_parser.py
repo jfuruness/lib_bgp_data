ac3baa527835866dde163dcdd5e7745b1b5cb3ad
Nicholas Shpetner <nicholas.shpetner@uconn.edu>
2020-07-21 11:38:13 -0400

    Now inits parser in tests instead of setup

--- lib_bgp_data/blacklist/tests/test_blacklist_parser.py
+++ lib_bgp_data/blacklist/tests/test_blacklist_parser.py
@@ -23,7 +23,6 @@ class Test_Blacklist_Parser:
     @pytest.fixture(autouse=True)
     def setup(self):
         """Parser setup and table deleted before every test"""
-        self.parser = Blacklist_Parser()
         # List of sources:
         self.lists = ['uce2', 'uce3', 'spamhaus', 'mit']
         with Database() as _db:
@@ -32,7 +31,8 @@ class Test_Blacklist_Parser:
     def test_get_blacklists(self):
         """This test makes sure that all blacklists were recieved and
         that there is some content or data."""
-        raw_data = self.parser._get_blacklists()
+        parser = Blacklist_Parser()
+        raw_data = parser._get_blacklists()
         for l in self.lists:
             # Make sure we got something
             assert raw_data[l] is not ''
@@ -44,7 +44,8 @@ class Test_Blacklist_Parser:
         """This test makes sure that the blacklists were parsed
         correctly, i.e. each key's items in dict are a list of strs
         of format ####### instead of AS#######"""
-        parsed = self.parser._parse_lists(self.test_get_blacklists())
+        parser = Blacklist_Parser()
+        parsed = parser._parse_lists(self.test_get_blacklists())
         for key in parsed.keys():
             for item in parsed[key]:
                 assert type(item) is str
@@ -55,7 +56,8 @@ class Test_Blacklist_Parser:
     def test_format_dict(self):
         """This test makes sure that the list before conversion to csv
         is formatted correctly, i.e. a list of [asn, source]"""
-        formatted = self.parser._format_dict(self.test_parse_lists())
+        parser = Blacklist_Parser()
+        formatted = parser._format_dict(self.test_parse_lists())
         for entry in formatted:
             assert type(entry[0]) is str
             assert len(entry[0]) > 0
@@ -67,9 +69,9 @@ class Test_Blacklist_Parser:
         """Test that should make sure that there are no errors and that
         the table is populated with the correct amount of ASNs"""
         # Run the parser
-        self.parser.run()
-        # Get the raw data. Done right after running parser for
-        # better precision.
+        parser = Blacklist_Parser()
+        parser.run()
+        # Get the raw data. 
         raw_dict = (self.test_parse_lists())
         # Get # of ASNs in each source
         raw_sizes = [len(raw_dict[source]) for source in raw_dict]
