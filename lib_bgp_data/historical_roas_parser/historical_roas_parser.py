dcbc3a526acc3d12307b0a33ea5a3aa2aaf5d9f3
Tony Zheng <mr.tonyzheng@gmail.com>
2020-08-17 17:06:55 -0400

    WIP

--- lib_bgp_data/historical_roas_parser/historical_roas_parser.py
+++ lib_bgp_data/historical_roas_parser/historical_roas_parser.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+Parser takes 10+ hours.
+Downloads 10,000+ csvs
+"""
+
 __author__ = "Tony Zheng"
 __credits__ = ["Tony Zheng"]
 __Lisence__ = "BSD"
@@ -10,10 +15,14 @@ __status__ = "Development"
 
 import os
 from ftplib import FTP, error_perm
+import threading
+from queue import Queue
+import time
+import concurrent.futures
 
 from ..base_classes import Parser
 from ..utils import utils
-from .tables import Historical_ROAS_Table
+from .tables import Historical_ROAS_Table, Historical_ROAS_Parsed_Table
 
 
 class Historical_ROAS_Parser(Parser):
@@ -21,12 +30,34 @@ class Historical_ROAS_Parser(Parser):
     count = 0
 
     def _run(self):
-        # context manager does ftp.quit()
+        """Collect the paths to all the csvs to download. Multithread the
+           downloading of all the csvs, insert into db if not seen before."""
+
+        paths = []
         with FTP('ftp.ripe.net') as ftp:
             ftp.login()
-            self.download_csvs(ftp, 'rpki')
+            self.get_csvs(ftp, 'rpki', paths)
+            paths = [(p, ftp) for p in paths]
 
-    def download_csvs(self, ftp, root):
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                executor.map(self.download_csv, paths)
+           
+        # context manager does ftp.quit()
+        #with FTP('ftp.ripe.net') as ftp:
+         #   ftp.login()
+          #  with Historical_ROAS_Table() as db:
+           #     self.download_csvs(ftp, 'rpki', db)
+            #    db.create_index()
+
+    def download_csv(self, path):
+        with open('temp.csv', 'wb') as f:
+            path[1].retrbinary(f'RETR {path[0]}', f.write())
+            self.reformat_csv(csv_path)
+        with Historical_ROAS_Parsed_Table() as db:
+            db.execute(f"INSERT INTO {db.name}(file) VALUES ({path})")
+
+    """
+    def download_csvs(self, ftp, root, db, paths):
         for path in ftp.nlst(root):
 
             # there are 3 possibilities:
@@ -35,27 +66,53 @@ class Historical_ROAS_Parser(Parser):
             # file is a folder - recurse deeper
 
             if 'csv' in path:
-                csv = 'temp.csv'
-                with open(csv, 'wb') as f:
-                    ftp.retrbinary(f'RETR {path}', f.write)
-                self.reformat_csv(csv)
-                utils.csv_to_db(Historical_ROAS_Table, csv)
-                self.count += 1
-                print(self.count)
+                paths.append(path)
+
+                with Historical_ROAS_Parsed_Table() as parsed:
+                    sql = f"SELECT * FROM {parsed.name} WHERE file = {path}" 
+                    if not parsed.get_count(sql=sql):
+                    csv = 'temp.csv'
+                    with open(csv, 'wb') as f:
+                        ftp.retrbinary(f'RETR {path}', f.write)
+                        self.reformat_csv(csv, path)
+                        utils.csv_to_db(Historical_ROAS_Table, csv)
+                        self.count += 1
+                        print(self.count, path)
+                        db.delete_duplicates()
+
             elif 'repo.tar.gz' in path:
                 pass
-            else:
-                self.download_csvs(ftp, path)
 
-    def reformat_csv(self, csv):
-        """Replace commas with tabs and remove the first column"""
+            else:
+                self.download_csvs(ftp, path, db)
+    """
+
+    def get_csvs(self, ftp, root, paths):
+        for path in ftp.nlst(root):
+            if 'csv' in path:
+                paths.append(path)
+            elif 'repo.tar.gx' in path:
+                pass
+            else:
+                self.get_csvs(ftp, path, paths)
+
+    def reformat_csv(self, csv, path):
+        """Replaces commas with tabs, removes URI column,
+           deletes 'AS' e.g. AS13335 -> 13335,
+           and adds date parsed from the file name."""
         with open(csv, 'r+') as f:
-            content = f.read()
-            # clear everything
+            content = f.read().split('\n')[1:]
+            # clear the file
             f.seek(0)
             f.truncate()
             for line in content:
-                f.write(''.join(line.split(',')[1:]).replace(',', '\t'))
-                
-                
+                if line:
+                    edited_line = line.split(',')[1:]
+                    edited_line[0] = edited_line[0][2:]
+                    f.write('\t'.join(edited_line))
+
+                    date = '-'.join(path.split('/')[2:5])
+                    f.write(f'\t{date}')
+                    f.write('\n')
+ 
             
--- lib_bgp_data/historical_roas_parser/tables.py
+++ lib_bgp_data/historical_roas_parser/tables.py
@@ -14,14 +14,50 @@ from ..database import Generic_Table
 class Historical_ROAS_Table(Generic_Table):
 
     name = "historical_roas"
+    columns = ['asn', 'prefix', 'maxlen', 'notbefore', 'notafter', 'date_added']
 
     def _create_tables(self):
-        sql = """CREATE UNLOGGED TABLE IF NOT EXISTS historical_roas (
-                 asn integer,
-                 prefix cidr,
-                 maxlength smallint,
-                 notbefore timestamp,
-                 notafter timestamp);"""
+        sql = f"""CREATE UNLOGGED TABLE IF NOT EXISTS {self.name} (
+                  asn bigint,
+                  prefix cidr,
+                  maxlen smallint,
+                  notbefore timestamp,
+                  notafter timestamp,
+                  date_added date);"""
+        self.execute(sql)
+
+    def delete_duplicates(self):
+        """Deletes duplicated roas, preserving earlier one."""
+
+        sql = f"""DELETE FROM {self.name} a
+                    USING     {self.name} b
+                  WHERE a.date_added > b.date_added
+                    AND a.asn = b.asn
+                    AND a.prefix = b.prefix
+                    AND a.maxlen = b.maxlen
+                    AND a.notbefore = b.notbefore
+                    AND a.notafter = b.notafter;"""
+        self.execute(sql)
+
+    def create_index(self):
+        index_name = f'{self.name}_index'
+
+        # if adding lots of data, it's faster to recreate than update
+        sql = f"DROP INDEX IF EXISTS {index_name}"
+        self.execute(sql)
+
+        sql = f"""CREATE INDEX IF NOT EXISTS {self.name}_index
+                    ON {self.name}(prefix, date_added)"""
+        self.execute(sql)
+
+class Historical_ROAS_Parsed_Table(Generic Table):
+
+    name = 'historical_roas_parse'
+    
+    def _create_tables(self):
+        sql = f"""CREATE UNLOGGED TABLE IF NOT EXISTS {self.name} (
+                    file text
+                    );"""
         self.execute(sql)
 
 
