74348b72ef56296723855d848a43e1a6d2c9ea92
Samarth Kasbawala <samarth.kasbawala@uconn.edu>
2020-08-18 15:02:18 -0400

    add changes suggested in pull request

--- lib_bgp_data/roas_parser/roas_automator.py
+++ lib_bgp_data/roas_parser/roas_automator.py
@@ -48,14 +48,22 @@ class BackupError(Exception):
     pass
 
 class ROAs_Automator(ROAs_Parser):
-    backup_dir = "/nightly_runs/roas_backup/"
-    temp_backup = f"{backup_dir}roas_temp.sql.gz"
-    prev_backup = f"{backup_dir}roas.sql.gz"
+    """ROAs_AUtomator class"""
+
+    # Define backup directories and make sure they exist
+    backup_dir = "/nightly_runs/roas_backup"
+    if not os.path.exists(backup_dir):
+        os.mkdir(backup_dir)
+    temp_backup = os.path.join(backup_dir, "roas_temp.sql.gz")
+    prev_backup = os.path.join(backup_dir, "roas.sql.gz")
  
     def _run(self):
         """Downloads and stores the roas from a json while properly maintaining
         the table in the database
         """
+
+        from ..database.config import global_section_header as gsh
+        assert gsh is not None
 
         with ROAs_Table() as table:
 
@@ -70,122 +78,99 @@ class ROAs_Automator(ROAs_Parser):
             except (psycopg2.errors.UndefinedTable, TableIsEmpty):
                 if os.path.exists(self.prev_backup):
                     table.clear_table()
-                    self._restore_table()
+                    table.restore_table(gsh, self.prev_backup)
                     table.create_index()
             finally:
                 # Get the roas that need to be inserted into the table
                 roas = self._format_roas(self._get_json_roas())
                 _csv_dir = f"{self.csv_dir}/roas.csv"
 
-                # Try backing up the table at most three times
+                # Try backing up the table at most three times. If
+                # unsuccessful, send an email indicating the error
                 try:
-                    for _ in range(3):
-                        # Insert the rows into the table
-                        utils.clean_paths([self.path, self.csv_dir])
-                        utils.rows_to_db(roas, _csv_dir,
-                                         ROAs_Table, clear_table=False)
-                        table.create_index()
-
-                        # Create a backup, if it is successful, overwrite the
-                        # existing backup
-                        if self._backup_table():
-                            self._overwrite()
-                            break
-
-                        # If the backup is unsuccessful, restore the table
-                        # to the last backup
-                        if os.path.exists(self.prev_backup):
-                            table.clear_table()
-                            self._restore_table()
-                            table.create_index()
-
-                        # Enters this block if there is no previous backup,
-                        # need to make the table empty so that data can tried
-                        # to be inserted again
-                        else:
-                            table.execute("DELETE FROM roas")
-                            
-                    # If after three attempts the backups are unsuccessful,
-                    # then just insert the new roas and don't backup.
+                    self._try_to_backup(roas, _csv_dir, table)
+                except (subprocess.CalledProcessError, BackupError) as e:
+                    subject = f"Roas Backup Error at {utils.now()}"
+                    if type(e) == BackupError:
+                        body = ("There was an error backing up the roas table. "
+                                "The backup file was not created correctly, so "
+                                "the previous backup was not overwritten. The "
+                                "pg_dump and pg_restore commands did not provide "
+                                "an error.")
                     else:
-                        utils.clean_paths([self.path, self.csv_dir])
-                        utils.rows_to_db(roas, _csv_dir,
-                                         ROAs_Table, clear_table=False)
-                        table.create_index()
-
-                        # Raise a backup error
-                        raise BackupError()
-
-                except subprocess.CalledProcessError as e:
-                    time = utils.now()
-                    subject = f"Roas Backup Error at {time}"
-                    body = ("There was an error backing up the roas table. "
-                            "Below are the outputs\n"
-                            f"STD_OUT Message: {e.stdout}\n"
-                            f"STD_ERR Message: {e.stderr}")
-                    utils.send_email(subject, body)
-
-                except BackupError:
-                    time = utils.now()
-                    subject = f"Roas Backup Error at {time}"
-                    body = ("There was an error backing up the roas table. "
-                            "The backup file was not created correctly, so "
-                            "the previous backup was not overwritten. The "
-                            "pg_dump and pg_restore commands did not provide "
-                            "an error.")
+                        body = ("There was an error backing up the roas table. "
+                                "Below are the outputs\n"
+                                f"STD_OUT Message: {e.stdout}\n"
+                                f"STD_ERR Message: {e.stderr}")
                     utils.send_email(subject, body)
 
 ########################
 ### Helper Functions ###
 ########################
 
-    def _backup_table(self):
-        """This funtion will backup the table and check the backup"""
+    def _try_to_backup(self, roas, csv_dir, table):
+        """Tries to backup table at most three times"""
 
-        # Get the number of rows in the table
-        with ROAs_Table() as table:
-            count = table.get_count()
-            
-        # Make a temporary backup
-        cmd = (f"sudo -i -u postgres "
-               f"pg_dump -Fc -t roas bgp > {self.temp_backup}")
-        subprocess.run(cmd, check=True, capture_output=True,
-                       shell=True, text=True)
+        from ..database.config import global_section_header as gsh
+        assert gsh is not None
 
-        # Restore the table from the temporary backup we just made
-        with ROAs_Table() as table:
-            try:
+        for _ in range(3):
+            # Insert the roas into the table
+            self._insert_rows(roas, csv_dir, table)
+
+            # Create a backup, if it is successful, overwrite the
+            # existing backup
+            table.backup_table(table.name, gsh, self.temp_backup)
+            if self._check_backup(table):
+                self._overwrite()
+                break
+
+            # If the backup is unsuccessful, restore the table
+            # to the last backup
+            if os.path.exists(self.prev_backup):
                 table.clear_table()
-                self._restore_table(temp=True)
+                table.restore_table(gsh, self.prev_backup)
                 table.create_index()
-            except:
-                # If there are any errors trying to restore the table,
-                # the backup was not successful
-                return False
 
-        # Get the new count of the table after the restore
-        with ROAs_Table() as table:
-            restore_count = table.get_count()
-
-        # Return True if the backup is consistent
-        return count == restore_count
-            
-    def _restore_table(self, temp=False):
-        """This function runs the command to restore the table from the
-        backup"""
-
-        cmd = "sudo -i -u postgres "
-        if temp:
-            cmd += f"pg_restore -d bgp {self.temp_backup}"
+            # Enters this block if there is no previous backup,
+            # need to make the table empty so that data can tried
+            # to be inserted again
+            else:
+                table.execute(f"DELETE FROM {table.name}")
+                
+        # If after three attempts the backups are unsuccessful,
+        # then just insert the new roas and don't backup.
         else:
-            cmd += f"pg_restore -d bgp {self.prev_backup}"
-        subprocess.run(cmd, check=True, capture_output=True,
-                       shell=True, text=True) 
+            self._insert_rows(roas, csv_dir, table)
+            raise BackupError()
+
+    def _insert_rows(self, roas, csv_dir, table):
+        """Inserts roas into table"""
+
+        utils.clean_paths([self.path, self.csv_dir])
+        utils.rows_to_db(roas, csv_dir, ROAs_Table, clear_table=False)
+        table.create_index()
+
+    def _check_backup(self, table):
+        """Checks whether or not the backup failed and returns a boolean"""
+
+        from ..database.config import global_section_header as gsh
+        assert gsh is not None
+
+        count = table.get_count()
+
+        try:
+            table.clear_table()
+            table.restore_table(gsh, self.temp_backup)
+            table.create_index()
+        except:
+            return False
+        else:
+            return count == table.get_count()
 
     def _overwrite(self):
         """This function overwrites the previous backup"""
 
         cmd = f"mv {self.temp_backup} {self.prev_backup}"
-        subprocess.run(cmd, check=True, capture_output=True,
-                       shell=True, text=True) 
+        utils.run_cmds(cmd)
 
