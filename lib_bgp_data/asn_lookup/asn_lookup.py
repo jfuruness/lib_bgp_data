8907bf79f42eea282463a11a02c0fc695c47ba6d
Tony Zheng <mr.tonyzheng@gmail.com>
2020-09-29 22:20:22 -0400

    new maxmind usage and cronjob util

--- lib_bgp_data/asn_lookup/asn_lookup.py
+++ lib_bgp_data/asn_lookup/asn_lookup.py
@@ -2,67 +2,76 @@
 # -*- coding: utf-8 -*-
 
 """
-Returns a list of the holding organization, countries, and continents for a
-list of ASNs.
+This module contains a function to get an ASN's country and continent.
 
-Using MaxMind database of CSVs which are updated weekly. But an account
-sign-up is necessary to download them, making the process manual.
-The benefit is that because they're CSVs, there is no ratelimit.
+The RIPE API is first used to get an IP address associated with the ASN.
+Then MaxMind's GeopIP2 API queries a downloaded database for the country
+and continent data.
 
-RIPE has an API with no ratelimit but it doesn't have good endpoints that
-return country and continent data. As a matter of fact, RIPE directly
-endorses MaxMind on their docs
+Setup guide:
+
+First you will need sign up for an account with MaxMind to use their data:
+https://www.maxmind.com/en/geolite2/signup
+
+Then you need to generate a config file on their website and paste it into
+/etc/GeoIP.conf
 """
 
+import os
+
+import geoip2.database
+import requests
+from ..utils import utils
+
 __author__ = "Tony Zheng"
-__credits__ = ["Tony Zheng, Reynaldo Morris"]
+__credits__ = ["Tony Zheng"]
 __Lisence__ = "BSD"
 __maintainer__ = "Justin Furuness"
 __email__ = "jfuruness@gmail.com"
 __status__ = "Development"
 
-import pandas as pd
+
+def asn_lookup(asn: int) -> dict:
+
+    assert type(asn) is int, "Not a number"
+
+    db = '/usr/share/GeoIP/GeoLite2-Country.mmdb'
+
+    assert os.path.exists('/etc/GeoIP.conf'), "Follow setup guide!"
+
+    if not os.path.exists(db):
+        install()
+
+    ripe = ('https://stat.ripe.net/data/announced-prefixes/',
+            f'data.json?resource=AS{asn}')
+
+    r = requests.get(ripe)
+    r.raise_for_status()
+    ip = r.json()['data']['prefixes'][0]['prefix'].split('/')[0]
+    r.close()
+
+    reader = geoip2.database.Reader(db)
+
+    response = reader.country(ip)
+
+    reader.close()
+
+    return response.country.iso_code, response.continent.code
 
 
-def asn_lookup(asns: list):
+def install():
+    """Installs the GeoIP Update tool (assuming Ubuntu system).
+       Also adds a cronjob to keep the database updated."""
 
-    infos = []
+    cmds = ['add-apt-repository ppa:maxmind/ppa',
+            'apt update',
+            'apt install geoipupdate']
 
-    # pandas to read the multiple csvs used
-    # I renamed the csvs because the original names were extremely long
-    asn_data = pd.read_csv('./ASN/ipv4.csv')
-    geo_data = pd.read_csv('./Country/country-ipv4.csv')
+    utils.run_cmds(cmds)
 
-    # keep_default_na must be turned off or else it interprets the
-    # abbr 'NA' for 'North America' as not a value
-    country_data = pd.read_csv('./Country/country-english.csv',
-                                keep_default_na=False)
-
-    for asn in asns:
-        # list of country and continent tuples
-        CC = []
-        rows = asn_data.loc[asn_data['autonomous_system_number'] == int(asn)]
-
-        for index, row in rows.iterrows():
-            org = row['autonomous_system_organization']
-            prefix = row['network']
-
-            geo_row = geo_data.loc[geo_data['network'] == prefix]
-
-            # some prefixes are not present
-            if not geo_row.empty:
-                gid = geo_row['geoname_id'].values[0]
-                country_row = country_data.loc[country_data['geoname_id'] \
-                                                == int(gid)]
-                country = country_row['country_iso_code'].values[0]
-                continent = country_row['continent_code'].values[0]
-                CC.append((country, continent))
-
-        infos.append({'org': org, 'country_continent': CC})
-
-    return infos
-
-if __name__ == '__main__':
-    print(asn_lookup(['13335']))
+    # MaxMind updates on Tuesdays, so we'll update on Wednesdays.
+    utils.add_cronjob('GeoIP_Update', 
+                      '0', '0', '*', '*', '3',
+                      '/etc/geoipupdate')
 
 
