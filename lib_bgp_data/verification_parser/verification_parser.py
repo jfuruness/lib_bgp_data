d091147eff3bd79696d5a6acfaf7cff3c7dad17c
Justin Furuness <jfuruness@gmail.com>
2020-11-18 19:27:41 -0500

    verification stats updates

--- lib_bgp_data/verification_parser/verification_parser.py
+++ lib_bgp_data/verification_parser/verification_parser.py
@@ -15,6 +15,7 @@ __maintainer__ = "Justin Furuness"
 __email__ = "jfuruness@gmail.com"
 __status__ = "Development"
 
+from .dataset_statistics_generator import Dataset_Statistics_Generator
 from .tables import Monitors_Table, Control_Monitors_Table
 
 from ..asrank_website_parser import ASRankWebsiteParser
@@ -22,6 +23,7 @@ from ..base_classes import Parser
 from ..database import Database
 from ..mrt_parser import MRT_Parser, MRT_Metadata_Parser, MRT_Sources
 from ..mrt_parser.tables import MRT_W_Metadata_Table
+from ..relationships_parser import Relationships_Parser
 
 class Verification_Parser(Parser):
     """This class generates input to the extrapolator verification
@@ -34,28 +36,34 @@ class Verification_Parser(Parser):
     def _run(self,
              test=False,
              clear_db=False,
+             relationships=False,
              mrt_announcements=False,
              as_rank=False,
              sample_selection=False,
+             dataset_stats=False,
              ):
         if clear_db and not test:
             assert False, "Clear db, checkpoint, vaccum analyze"
+        if relationships:
+            Relationships_Parser(**self.kwargs)._run()
         if mrt_announcements:
             kwargs = {"sources": [MRT_Sources.RIPE, MRT_Sources.ROUTE_VIEWS],
                       "IPV4": True,
                       "IPV6": False}
             if test:
                 kwargs["api_param_mods"] = {"collectors[]": ["route-views2"]}
-            MRT_Parser(**self.kwargs).run(**kwargs)
-            MRT_Metadata_Parser(**self.kwargs).run(**kwargs)
+            MRT_Parser(**self.kwargs)._run(**kwargs)
+            MRT_Metadata_Parser(**self.kwargs)._run(**kwargs)
             with MRT_W_Metadata_Table() as db:
                 sql = """CREATE INDEX monitor_btree
                         ON {db.name}(monitor_asn);"""
                 db.execute(sql)
         if as_rank:
-            ASRankWebsiteParser(**self.kwargs).run()
+            ASRankWebsiteParser(**self.kwargs)._run()
         if sample_selection:
             # Fills monitor stats and control table
             for Table in [Monitors_Table, Control_Monitors_Table]:
                 with Table(clear=True) as db:
                     db.fill_table()
+        if dataset_stats:
+            Dataset_Statistics_Generator(**self.kwargs)._run()
