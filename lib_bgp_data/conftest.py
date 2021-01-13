fdb92f02b4ec975cc5470202cf7d44001321693a
Tony Zheng <mr.tonyzheng@gmail.com>
2021-01-13 14:39:27 -0500

    pytest session creates/drops db, individual tests drop public schema

--- lib_bgp_data/conftest.py
+++ lib_bgp_data/conftest.py
@@ -8,6 +8,7 @@ from subprocess import run, check_call
 from datetime import datetime
 from .utils.database import config
 from .utils.database import Postgres
+from .utils.database import Database
 from .utils import utils
 
 __author__ = ["Justin Furuness", "Tony Zheng"]
@@ -20,21 +21,36 @@ __status__ = "Development"
 
 import os
 
-def pytest_runtest_setup():
+# these are called initialization hooks
+# https://docs.pytest.org/en/latest/reference.html#initialization-hooks
+
+def pytest_sessionstart():
     # used for postgres restart command in config.py
     # and random page cost and ulimit in postgres.py
-    # https://docs.pytest.org/en/6.0.1/example/simple.html#pytest-current-test-environment-variable
+    # https://docs.pytest.org/en/latest/example/simple.html#pytest-current-test-environment-variable
     os.environ["PYTEST_CURRENT_TEST"] = "why doesn't this work"
     
     # Underscores are like the only character I can use here that SQL allows
     section = test_prepend() + datetime.now().strftime(test_db_fmt())
     config.Config(section).install()
     config.global_section_header = section
- 
-def pytest_runtest_teardown():
+
+def pytest_sessionfinish():
     drop_old_test_databases()
     section = config.global_section_header
     Postgres.erase_db(section)
+
+def pytest_runtest_teardown():
+
+    # https://stackoverflow.com/questions/3327312/how-can-i-drop-all-the-tables-in-a-postgresql-database
+    cmds = ['DROP SCHEMA public CASCADE;',
+            'CREATE SCHEMA public;',
+            'GRANT ALL ON SCHEMA public TO postgres;',
+            'GRANT ALL ON SCHEMA public TO public;']
+
+    with Database() as db:
+        for cmd in cmds:
+            db.execute(cmd)
 
 def drop_old_test_databases():
     # Look for and drop any test dbs that are older than 1 week
