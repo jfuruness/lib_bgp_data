bcb5172bd2d7f8943725fcb18874fb295552fed8
Nicholas Shpetner <nicholas.shpetner@uconn.edu>
2021-02-03 12:52:51 -0500

    In-progress commit of tests pre-pull from origin/master

--- lib_bgp_data/simulations/simulator/tests/test_simulator.py
+++ lib_bgp_data/simulations/simulator/tests/test_simulator.py
@@ -17,11 +17,14 @@ __status__ = "Development"
 import pytest
 
 from ..simulator import Simulator
-
+from ..simulator import Simulation_Results_Table
+from ..attacks.attack import Attack
+from ...enums import Enums
 from ...utils import utils
 from ...utils.database import Database
+from ...utils.postgres import Postgres
 
-@pytest.mark.skip(reason="undergrad work")
+
 @pytest.mark.simulator
 class Test_Simulator:
     """Tests all functionality that the simulator class offers."""
@@ -32,7 +35,7 @@ class Test_Simulator:
         Perhaps use the postgres/database convenience funcs for this?
         """
 
-        pass
+        # Postgres.erase_all()
 
     def test_run(self):
         """Tests _run function with default parameters
@@ -40,7 +43,7 @@ class Test_Simulator:
         All combinations of parameters are not tested
         since that would take too long to be useful"""
 
-        pass
+        Simulator._run()
 
     def test_exr_bash(self):
         """Tests _run function with a different exr_bash
@@ -48,16 +51,28 @@ class Test_Simulator:
         Should test that exr_bash changes the running of the exr
         probably just echo something
         """
+        #TODO: Just use echo, try to capture stdout, confirm it works
+        # May error with echo, use try/catch
+        Simulator._run(ext_bash.....
+        # OK, so ext_bash seems to affect a datapoint in data_point.py, which in get_data will use exr_bash in Parser.run
+        # (see line 83)
+        # But I don't have any further information beyond that, what am I working with here?
 
-        pass
-
+    @pytest.mark.det
     def test_deterministic(self):
         """Tests deterministic trials
 
         Trials should run the same way every time
         """
 
-        pass
+        # Copy table, run again, compare copies?
+        with Simulation_Results_Table() as table:                
+            Simulator._run(deterministic=True)
+            run_one = table.get_all()
+            table.clear_table()
+            Simulator._run(deterministic=True)
+            run_two = table.get_all()
+            assert run_one == run_two
 
     def test_seeded_trial(self):
         """Tests the seeded trial
@@ -65,7 +80,8 @@ class Test_Simulator:
         Should jump to the trial displayed in the deterministic trial
         """
 
-        pass
+        # Not sure how to use the seeded arg here, either
+        # TODO: Should jump to a trial in deterministic
 
     def test_all_scenarios(self):
         """Tests running of all attack types and adopt policies
@@ -73,12 +89,18 @@ class Test_Simulator:
         percents should be 1, 50, 90. Trials should be 2
         """
 
-        pass
+        percents = [1, 50, 90]
+        for i in range(0, 2):
+            #TODO: Add percents
+            Simulator.run(attack_types=Attack.runnable_attacks,
+                          adopt_policies=list(Enums.Non_Default_Policies.__members__.values()))
 
     def test_percents_trials(self):
         """Test with percents as [1, 25, 50, 75, 100] for 4 trials"""
 
-        pass
+        percents = [1, 25, 50, 75, 100]
+        for i in range(0, 4):
+            Simulator._run(percents=percents)
 
     def test_redownload_base_data(self):
         """Tests the redownloading of base data function
@@ -90,10 +112,15 @@ class Test_Simulator:
         Make sure AS Rank Website Parser was run
             -make sure that there is a random delay
         Make sure ASes Table has an index
-        Make sure AS Rank Table has an index
         """
-
-        pass
+        #TODO: Add Exr_Cls=Sim_Exr to _redownload_base in simulator.py, don't worry after that
+        Simulator._redownload_base_data(something)
+        with Database() as db:
+            # TODO: Import table, don't use hardcoded name
+            assert db.execute("FROM as_rank SELECT * ") is not None
+            assert db.execute("FROM ases SELECT * ") is not None
+            assert db.execute("SELECT tablename, indexname FROM pg_indexes WHERE tablename = as_rank") is not None
+            assert db.execute("SELECT tablename, indexname FROM pg_indexes WHERE tablename = ases") is not None
 
     def test_total(self):
         """Tests the _total function
@@ -101,13 +128,5 @@ class Test_Simulator:
         Make sure the total is as expected for a small test case
         """
 
-        pass
-
-    def test_exr_kwargs(self):
-        """Test _exr_kwargs function
-
-        Just make sure exr_branch is added if not None
-        (test for both None and specified)
-        """
-
-        pass
+        total = Simulator._total()
+        assert Total = 99999
