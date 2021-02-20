a0cebd07e91497ca8d1d30607cf383e70f7218c2
Nicholas Shpetner <nicholas.shpetner@uconn.edu>
2021-02-19 19:07:23 -0500

    Adds most of the tests. Some are still yet to be completed

--- lib_bgp_data/simulations/simulator/tests/test_simulator.py
+++ lib_bgp_data/simulations/simulator/tests/test_simulator.py
@@ -19,11 +19,14 @@ import pytest
 from ..simulator import Simulator
 from ..simulator import Simulation_Results_Table
 from ..attacks.attack import Attack
-from ...enums import Enums
-from ...utils import utils
-from ...utils.database import Database
-from ...utils.postgres import Postgres
-
+from ...enums import Non_Default_Policies
+from ....utils import utils
+from ....utils.database import Database
+from ....utils.database.postgres import Postgres
+from ....collectors.relationships.relationships_parser import Relationships_Parser
+from ....collectors.as_rank_website.as_rank_website_parser import AS_Rank_Website_Parser
+from ....collectors.as_rank_website.tables import AS_Rank_Table
+from ....collectors.relationships.tables import ASes_Table
 
 @pytest.mark.simulator
 class Test_Simulator:
@@ -34,8 +37,8 @@ class Test_Simulator:
 
         Perhaps use the postgres/database convenience funcs for this?
         """
-
-        # Postgres.erase_all()
+        Relationships_Parser()._run()
+        AS_Rank_Website_Parser()._run()
 
     def test_run(self):
         """Tests _run function with default parameters
@@ -43,7 +46,8 @@ class Test_Simulator:
         All combinations of parameters are not tested
         since that would take too long to be useful"""
 
-        Simulator._run()
+        exr_bash = self.prep_exr()
+        Simulator()._run(redownload_base_data=True, exr_bash=exr_bash)
 
     def test_exr_bash(self):
         """Tests _run function with a different exr_bash
@@ -53,24 +57,24 @@ class Test_Simulator:
         """
         #TODO: Just use echo, try to capture stdout, confirm it works
         # May error with echo, use try/catch
-        Simulator._run(ext_bash.....
+        #Simulator._run(
         # OK, so ext_bash seems to affect a datapoint in data_point.py, which in get_data will use exr_bash in Parser.run
         # (see line 83)
         # But I don't have any further information beyond that, what am I working with here?
 
-    @pytest.mark.det
     def test_deterministic(self):
         """Tests deterministic trials
 
         Trials should run the same way every time
         """
 
+        exr_bash = self.prep_exr()
         # Copy table, run again, compare copies?
         with Simulation_Results_Table() as table:                
-            Simulator._run(deterministic=True)
+            Simulator()._run(deterministic=True, redownload_base_data=False, exr_bash=exr_bash)
             run_one = table.get_all()
             table.clear_table()
-            Simulator._run(deterministic=True)
+            Simulator()._run(deterministic=True, redownload_base_data=False, exr_bash=exr_bash)
             run_two = table.get_all()
             assert run_one == run_two
 
@@ -88,19 +92,20 @@ class Test_Simulator:
 
         percents should be 1, 50, 90. Trials should be 2
         """
-
+        exr_bash = self.prep_exr()
         percents = [1, 50, 90]
         for i in range(0, 2):
-            #TODO: Add percents
-            Simulator.run(attack_types=Attack.runnable_attacks,
-                          adopt_policies=list(Enums.Non_Default_Policies.__members__.values()))
+            Simulator().run(attack_types=Attack.runnable_attacks,
+                            adopt_policies=list(Non_Default_Policies.__members__.values()),
+                            percents=percents,
+                            exr_bash=exr_bash)
 
     def test_percents_trials(self):
         """Test with percents as [1, 25, 50, 75, 100] for 4 trials"""
-
+        exr_bash = self.prep_exr()
         percents = [1, 25, 50, 75, 100]
         for i in range(0, 4):
-            Simulator._run(percents=percents)
+            Simulator()._run(percents=percents, exr_bash=exr_bash)
 
     def test_redownload_base_data(self):
         """Tests the redownloading of base data function
@@ -113,20 +118,31 @@ class Test_Simulator:
             -make sure that there is a random delay
         Make sure ASes Table has an index
         """
-        #TODO: Add Exr_Cls=Sim_Exr to _redownload_base in simulator.py, don't worry after that
-        Simulator._redownload_base_data(something)
-        with Database() as db:
-            # TODO: Import table, don't use hardcoded name
-            assert db.execute("FROM as_rank SELECT * ") is not None
-            assert db.execute("FROM ases SELECT * ") is not None
-            assert db.execute("SELECT tablename, indexname FROM pg_indexes WHERE tablename = as_rank") is not None
-            assert db.execute("SELECT tablename, indexname FROM pg_indexes WHERE tablename = ases") is not None
+        Simulator()._redownload_base_data()
+        with ASes_Table() as db:
+            assert db.execute(f"SELECT * FROM {db.name}") is not None
+            assert db.execute(f"SELECT tablename, indexname FROM pg_indexes WHERE tablename = '{db.name}' ;") is not None
+        with AS_Rank_Table() as db:
+            assert db.execute(f"SELECT * FROM {db.name}") is not None
+            assert db.execute(f"SELECT tablename, indexname FROM pg_indexes WHERE tablename = '{db.name}' ;") is not None
 
     def test_total(self):
         """Tests the _total function
 
         Make sure the total is as expected for a small test case
         """
+        return 0
 
-        total = Simulator._total()
-        assert Total = 99999
+    ##################
+    #Helper functions#
+    ##################
+    def prep_exr(self):
+        cur_db = ''
+        with Database() as db:
+            cur_db = db.execute('SELECT current_database();')
+            cur_db = cur_db[0]['current_database']
+            print(cur_db)
+        exr_bash = '/usr/bin/rovpp_compat_modifications_extrapolator -v 1 -i 0 -b 0 -a mrt_w_metadata  -t top_100_ases -t edge_ases -t etc_ases --rounds 1 --config-section ' + cur_db
+        return exr_bash
+ 
+
