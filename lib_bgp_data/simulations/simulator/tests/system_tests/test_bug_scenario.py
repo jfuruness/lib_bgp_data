4e44b68f5d9c56835a63d5fc2b329dd083ad3eb1
Justin Furuness <jfuruness@gmail.com>
2021-03-03 22:32:32 -0500

    simulations stuff

--- lib_bgp_data/simulations/simulator/tests/system_tests/test_bug_scenario.py
+++ lib_bgp_data/simulations/simulator/tests/system_tests/test_bug_scenario.py
@@ -12,12 +12,12 @@ import pytest
 from .graph_tester import Graph_Tester
 #from ..tables import Hijack
 from ....enums import Non_Default_Policies, Policies, Data_Plane_Conditions as Conds
-from ...attacks.attack_classes import Unannounced_Prefix_Superprefix_Hijack
+from ...attacks.attack_classes import Prefix_Superprefix_Hijack
 from ...attacks.attack import Attack
 
 
-__author__ = "Justin Furuness"
-__credits__ = ["Justin Furuness"]
+__author__ = "Reynaldo Morillo"
+__credits__ = ["Reynaldo Morillo"]
 __Lisence__ = "BSD"
 __maintainer__ = "Justin Furuness"
 __email__ = "jfuruness@gmail.com"
@@ -32,9 +32,11 @@ class Test_Bug_Scenario(Graph_Tester):
         r"""
 
 	TODO: create a png of scenario and keep in this directory.
+        Link to scenario depiction (last slide)
+        https://docs.google.com/presentation/d/14N7Jifs5ExPKrdWTabSjjbG8QW6R8us91bJpK8ZkRXk/edit?usp=sharing
         """
 
-        attack_types = [Unannounced_Prefix_Superprefix_Hijack]
+        attack_types = [Prefix_Superprefix_Hijack]
         adopt_policies = [Non_Default_Policies.ROVPP_V1]
         peer_rows = [[2914, 1299],
 		     [24875, 52320],
@@ -51,7 +53,7 @@ class Test_Bug_Scenario(Graph_Tester):
                                   [268337, 269494],
 				  [12389, 33892]]
         # Set adopting rows
-        bgp_ases = [174, 31133, 2914, 52320, 1299, 12389]
+        bgp_ases = [174, 31133, 2914, 52320, 1299, 12389, 33892]
         rov_adopting_ases = []
         rovpp_adopting_ases = [213371, 208673, 24875, 53180, 268337, 269494]
         adopting_rows = []
@@ -72,7 +74,7 @@ class Test_Bug_Scenario(Graph_Tester):
 		      {"asn": 269494,
                        "prefix": Attack.default_superprefix,
                        "origin": 33892,
-                       "received_from_asn": Conds.NOTHIJACKED.value},
+                       "received_from_asn": 268337},
                       {"asn": 268337,
                        "prefix": Attack.default_prefix,
                        "origin": 269494,
@@ -83,11 +85,11 @@ class Test_Bug_Scenario(Graph_Tester):
                        "received_from_asn": 53180},
 		      {"asn": 53180,
                        "prefix": Attack.default_prefix,
-                       "origin": 268337,
+                       "origin": 269494,
                        "received_from_asn": 268337},
 		      {"asn": 53180,
                        "prefix": Attack.default_superprefix,
-                       "origin": 52320,
+                       "origin": 33892,
                        "received_from_asn": 52320},
 		      {"asn": 52320,
                        "prefix": Attack.default_prefix,
