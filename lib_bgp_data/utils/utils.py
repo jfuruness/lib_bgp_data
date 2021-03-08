8ce6560e7dd1ac5790b29c3b4deff7ecab31b703
Samarth Kasbawala <samarth.kasbawala@uconn.edu>
2021-03-07 20:58:33 -0500

    fix add_cronjob function

--- lib_bgp_data/utils/utils.py
+++ lib_bgp_data/utils/utils.py
@@ -406,5 +406,5 @@ def kill_port(port: int, wait: bool = True):
 def add_cronjob(name, time, executable, overwrite=False):
     """Creates a cronjob of name, that runs executable at (cron) time."""
     cronjob = f'/etc/cron.d/{name}'
-    if not os.path.exists or overwrite:
-        run_cmds(f'echo "{time} root {executable} > {cronjob}')
+    if not os.path.exists(cronjob) or overwrite:
+        run_cmds(f'sudo echo "{time} root {executable}" > {cronjob}')
