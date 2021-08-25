1b67f8b9b4300e22cea6656565d03e9ed046cb4e
nickup9 <nicholas.shpetner9@gmail.com>
2021-08-25 14:05:16 +0000

    Modifes for v2

--- lib_bgp_data/collectors/as_rank_v2/tests/test_tables.py
+++ lib_bgp_data/collectors/as_rank_v2/tests/test_tables.py
@@ -5,21 +5,22 @@
 For specifics on each test, see the docstrings under each function.
 """
 
-__authors__ = ["Abhinna Adhikari"]
-__credits__ = ["Abhinna Adhikari"]
+__authors__ = ["Nicholas Shpetner"]
+__credits__ = ["Nicholas Shpetner"]
 __Lisence__ = "BSD"
-__maintainer__ = "Abhinna Adhikari"
-__email__ = "abhinna.adhikari@uconn.edu"
+__maintainer__ = "Justin Furuness"
+__email__ = "jfuruness@gmail.com"
 __status__ = "Development"
 
 import pytest
 
 from ....utils.database import Generic_Table_Test
-from ..tables import AS_Rank_Table
+from ..as_rank_v2_parser import AS_Rank_Parser_V2
+from ..tables import AS_Rank_V2
 
 
-@pytest.mark.asrank_website_parser
-class Test_AS_Rank_Table(Generic_Table_Test):
-    """Tests AS_Rank_Table."""
+@pytest.mark.asrank_parser_v2
+class Test_AS_Rank_V2(Generic_Table_Test):
+    """Tests AS_Rank_V2"""
 
-    table_class = AS_Rank_Table
+    table_class = AS_Rank_V2
