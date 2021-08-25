a3795d5da2557d483fb2695e7dae85eb5e0091e1
nickup9 <nicholas.shpetner9@gmail.com>
2021-08-25 13:51:30 +0000

    Adds quicker test to check db insertion, moves old test, edit attribution

--- lib_bgp_data/collectors/as_rank_v2/tests/test_asrank_parser_v2.py
+++ lib_bgp_data/collectors/as_rank_v2/tests/test_asrank_parser_v2.py
@@ -5,14 +5,13 @@
 For specifics on each test, see the docstrings under each function.
 """
 
-__authors__ = ["Nicholas Shpetner", "Abhinna Adhikari"]
-__credits__ = ["Nicholas Shpetner", "Abhinna Adhikari"]
+__authors__ = ["Nicholas Shpetner"]
+__credits__ = ["Nicholas Shpetner"]
 __Lisence__ = "BSD"
-__maintainer__ = "Abhinna Adhikari"
-__email__ = "abhinna.adhikari@uconn.edu"
+__maintainer__ = "Justin Furuness"
+__email__ = "jfuruness@gmail.com"
 __status__ = "Development"
 
-from unittest.mock import patch
 import pytest
 
 from ..as_rank_v2_parser import AS_Rank_Parser_V2
@@ -21,11 +20,33 @@ from ..tables import AS_Rank_V2
 @pytest.mark.asrank_parser_v2
 class Test_AS_Rank_Parser_V2:
 
-    def test_run(self):
+    def test_quick_run(self):
+        """Checks the first 10 ranked ASNs and makes sure everything
+        is formatted properly and inserted"""
         parser = AS_Rank_Parser_V2()
-        parser._run(1, 50)
+        parser._run(0, 10)
         with AS_Rank_V2() as db:
             result = db.execute('SELECT count(*) FROM as_rank_v2')
-            print(result)
-            # Temporary hardcoding
-            assert result == 101276
+            assert result[0]['count'] == 10
+            first_row = db.execute('SELECT * FROM as_rank_v2 LIMIT 1')
+            first_row = first_row[0]
+            links = first_row['links']
+            org = first_row['organization']
+            rank = first_row['rank']
+            assert links is not None
+            for link in links:
+                assert type(link) == int
+            assert type(org) == str
+            assert rank == 1
+
+    @pytest.mark.slow
+    def test_full(self):
+        """This does not verify formatting, quick_run does that. This
+        instead makes sure that the parser pulls all ASNs from Caida.
+        Also this is INCREDIBLY SLOW, takes 6-ish hours to complete"""
+        parser = AS_Rank_Parser_V2()
+        count = parser._run() - 1
+        with AS_Rank_V2() as db:
+            result = db.execute('SELECT count(*) FROM as_rank_v2')
+            assert result[0]['count'] == count
+
